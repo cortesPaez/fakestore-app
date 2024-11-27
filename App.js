b8a5fe8b1352eb36257d78d41aaf5d6e41db3585
ccortesdev <cristobal.cortes@aeolabs.io>
2024-11-27 15:03:33 -0300

    feat: added react navigator and query client

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import SignInScreen from "./screens/SignInScreen";
+import { QueryClient, QueryClientProvider } from "react-query";
+import ProductList from "./screens/ProductList";
+import ProductDetail from "./screens/ProductDetail";
 
+const Stack = createNativeStackNavigator();
+const queryClient = new QueryClient();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Sign In" component={SignInScreen} />
+          <Stack.Screen
+            name="Products List"
+            component={ProductList}
+            options={{ headerBackVisible: false }}
+          />
+          <Stack.Screen name="Product Detail" component={ProductDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
